c7d9a235b784c731e1beb1f53baaa9149cf3226f
Ruwaid Louis <ruwaid@kth.se>
2020-02-23 09:43:23 +0100

    ADD get_attributes function to logic

--- wemake_python_styleguide/logic/tree/classes.py
+++ wemake_python_styleguide/logic/tree/classes.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from typing import Optional
+import ast
+from typing import List, Optional, Tuple
 
+from wemake_python_styleguide import types
+from wemake_python_styleguide.compat.aliases import AssignNodes
 from wemake_python_styleguide.constants import ALLOWED_BUILTIN_CLASSES
+from wemake_python_styleguide.logic import nodes
 from wemake_python_styleguide.logic.naming.builtins import is_builtin_name
 
 
@@ -34,3 +38,25 @@ def is_forbidden_super_class(class_name: Optional[str]) -> bool:
     if class_name in ALLOWED_BUILTIN_CLASSES:
         return False
     return is_builtin_name(class_name)
+
+
+def get_attributes(
+    node: ast.ClassDef,
+) -> Tuple[List[types.AnyAssign], List[ast.Attribute]]:
+    """Returns all class and instance attributes of a class."""
+    class_attributes = []
+    instance_attributes = []
+
+    for nd in ast.walk(node):
+        if isinstance(nd, ast.Attribute) and isinstance(nd.ctx, ast.Store):
+            instance_attributes.append(nd)
+            continue
+
+        has_assign = (
+            nodes.get_context(nd) == node and
+            getattr(nd, 'value', None)
+        )
+        if isinstance(nd, AssignNodes) and has_assign:
+            class_attributes.append(nd)
+
+    return class_attributes, instance_attributes
