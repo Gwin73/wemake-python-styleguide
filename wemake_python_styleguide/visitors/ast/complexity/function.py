1a26d22cc8c29d2c18ecec77ff4e0c140dc21b46
iZafiro <fabian.levican@sansano.usm.cl>
2020-02-24 21:19:41 -0300

    Fix: Added _ at the beginning of a public attribute in class _ComplexityCounter(object). Issue #14

--- wemake_python_styleguide/visitors/ast/complexity/function.py
+++ wemake_python_styleguide/visitors/ast/complexity/function.py
@@ -64,7 +64,7 @@ class _ComplexityCounter(object):
         self.variables: DefaultDict[AnyFunctionDef, List[str]] = defaultdict(
             list,
         )
-        self.exit_metrics: _ComplexityExitMetrics()
+        self._exit_metrics: _ComplexityExitMetrics()
 
     def check_arguments_count(self, node: AnyFunctionDefAndLambda) -> None:
         """Checks the number of the arguments in a function."""
@@ -111,11 +111,11 @@ class _ComplexityCounter(object):
                 self._update_variables(node, sub_node)
 
         error_counters: _NodeTypeHandler = {
-            ast.Return: self.exit_metrics.returns,
+            ast.Return: self._exit_metrics.returns,
             ast.Expr: self.expressions,
             ast.Await: self.awaits,
             ast.Assert: self.asserts,
-            ast.Raise: self.exit_metrics.raises,
+            ast.Raise: self._exit_metrics.raises,
         }
 
         for types, counter in error_counters.items():
@@ -211,7 +211,7 @@ class FunctionComplexityVisitor(BaseNodeVisitor):
                 TooManyArgumentsViolation,
             ),
             (
-                self._counter.exit_metrics.returns,
+                self._counter._exit_metrics.returns,
                 self.options.max_returns,
                 TooManyReturnsViolation,
             ),
@@ -226,7 +226,7 @@ class FunctionComplexityVisitor(BaseNodeVisitor):
                 TooManyAssertsViolation,
             ),
             (
-                self._counter.exit_metrics.raises,
+                self._counter._exit_metrics.raises,
                 self.options.max_raises,
                 TooManyRaisesViolation,
             ),
